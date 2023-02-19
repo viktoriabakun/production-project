756fc0c9dd8fd24852d37fd3751a08b09df35e8b
Viktoryia <viktoriabakun00@gmail.com>
2023-02-19 22:47:20 +0100

    feat: add stylelint, eslint i18next plugin, refactor package.json

--- src/widgets/Navbar/ui/index.tsx
+++ src/widgets/Navbar/ui/index.tsx
@@ -1,17 +1,22 @@
 import { FC } from 'react';
 import cn from 'shared/lib/classNames';
 import { APP_LINK_THEME, AppLink } from 'shared/ui/AppLink';
+import { useTranslation } from 'react-i18next';
 import styles from './styles.module.scss';
 
 interface INavbar {
     className?: string;
 }
 
-export const Navbar: FC<INavbar> = ({ className }) => (
-    <div className={cn(styles.navbar, {}, [className])}>
-        <div className={styles.links}>
-            <AppLink theme={APP_LINK_THEME.SECONDARY} to="/">Main</AppLink>
-            <AppLink theme={APP_LINK_THEME.SECONDARY} to="about">About</AppLink>
+export const Navbar: FC<INavbar> = ({ className }) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={cn(styles.navbar, {}, [className])}>
+            <div className={styles.links}>
+                <AppLink theme={APP_LINK_THEME.SECONDARY} to="/">{t('Main')}</AppLink>
+                <AppLink theme={APP_LINK_THEME.SECONDARY} to="about">{t('About')}</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
