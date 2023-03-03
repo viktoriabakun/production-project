76f04ab59e0ccb7b7f373f992de0eeadf7c6a965
Viktoryia <viktoriabakun00@gmail.com>
2023-03-03 16:02:28 +0100

    feat: add error to test sentry

--- src/pages/About/ui/index.tsx
+++ src/pages/About/ui/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
+
+    useEffect(() => {
+        throw new Error('haha you failed');
+    }, []);
 
     return (
         <div>
