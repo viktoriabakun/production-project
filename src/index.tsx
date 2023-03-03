f07512cebad5e639c045b25acc864b0ba9a621b7
Viktoryia <viktoriabakun00@gmail.com>
2023-03-03 15:38:54 +0100

    feat: add sentry for logging errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import { render } from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from 'app/providers/ThemeProvider';
 import App from './app/App';
 import 'shared/config/i18n';
+
+Sentry.init({
+    dsn: 'https://846374e59f56449582dd7e7e5c2b48e8@o4504775140769792.ingest.sentry.io/4504775144308736',
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 render(
     <BrowserRouter>
