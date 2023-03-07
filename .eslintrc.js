7f10699e8cc9468d9ba13e7c7ab9db8d3da689ab
Viktoryia <viktoriabakun00@gmail.com>
2023-03-07 18:41:15 +0100

    feat: add storybook, move to config folder, change main.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,57 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:i18next/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        'i18next',
-    ],
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'i18next'],
+  rules: {
+    indent: [2, 4],
+    'react/jsx-indent': [2, 4],
+    'react/jsx-indent-props': [2, 4],
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.tsx']
+    }],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': ['warn', {
+      devDependencies: true
+    }],
+    'no-restricted-exports': 'warn',
+    'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['error', {
+      markupOnly: true,
+      ignoreAttribute: ['data-testid', 'to']
+    }],
+    'max-len': ['error', {
+      code: 100,
+      ignoreComments: true,
+      ignoreUrls: true
+    }]
+  },
+  globals: {
+    __IS_DEV_BUILD: true
+  },
+  overrides: [{
+    files: ['**/src/**/*.test.{ts,tsx}'],
     rules: {
-        indent: [2, 4],
-        'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
-        'import/no-unresolved': 'off',
-        'import/prefer-default-export': 'off',
-        'no-unused-vars': 'warn',
-        'react/require-default-props': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'warn',
-        'react/function-component-definition': 'off',
-        'no-shadow': 'off',
-        'import/extensions': 'off',
-        'import/no-extraneous-dependencies': ['warn', { devDependencies: true }],
-        'no-restricted-exports': 'warn',
-        'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': [
-            'error',
-            {
-                markupOnly: true, ignoreAttribute: ['data-testid'],
-            },
-        ],
-        'max-len': ['error', { code: 100, ignoreComments: true, ignoreUrls: true }],
-    },
-    globals: {
-        __IS_DEV_BUILD: true,
-    },
-    overrides: [
-        {
-            files: ['**/src/**/*.test.{ts,tsx}'],
-            rules: {
-                'i18next/no-literal-string': 'off',
-            },
-        },
-    ],
+      'i18next/no-literal-string': 'off'
+    }
+  }]
 };
