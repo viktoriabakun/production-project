55ef6c956e3d644c05cbd5c5b94b75d1003ea680
Viktoryia <viktoriabakun00@gmail.com>
2023-03-09 18:30:27 +0100

    feat: move links from navbar to sidebar, add componentRender for unit tests

--- src/widgets/Navbar/ui/index.tsx
+++ src/widgets/Navbar/ui/index.tsx
@@ -1,22 +1,15 @@
 import { FC } from 'react';
 import cn from 'shared/lib/classNames';
-import { APP_LINK_THEME, AppLink } from 'shared/ui/AppLink';
-import { useTranslation } from 'react-i18next';
 import styles from './styles.module.scss';
 
 interface INavbar {
     className?: string;
 }
 
-export const Navbar: FC<INavbar> = ({ className }) => {
-    const { t } = useTranslation();
-
-    return (
-        <div className={cn(styles.navbar, {}, [className])}>
-            <div className={styles.links}>
-                <AppLink theme={APP_LINK_THEME.SECONDARY} to="/">{t('Main')}</AppLink>
-                <AppLink theme={APP_LINK_THEME.SECONDARY} to="about">{t('About')}</AppLink>
-            </div>
+export const Navbar: FC<INavbar> = ({ className }) => (
+    <div className={cn(styles.navbar, {}, [className])}>
+        <div className={styles.links}>
+            -
         </div>
-    );
-};
+    </div>
+);
--- src/widgets/Sidebar/ui/Sidebar/index.tsx
+++ src/widgets/Sidebar/ui/Sidebar/index.tsx
@@ -3,6 +3,11 @@ import cn from 'shared/lib/classNames';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { Button, BUTTON_SIZE, BUTTON_THEME } from 'shared/ui/Button';
+import { APP_LINK_THEME, AppLink } from 'shared/ui/AppLink';
+import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/route';
+import AboutIcon from 'shared/assets/icons/about.svg';
+import MainIcon from 'shared/assets/icons/home.svg';
 import styles from './styles.module.scss';
 
 interface ISidebar {
@@ -10,6 +15,7 @@ interface ISidebar {
 }
 
 export const Sidebar: FC<ISidebar> = ({ className }) => {
+    const { t } = useTranslation();
     const [isCollapsed, setIsCollapsed] = useState(false);
 
     const onToggle = () => setIsCollapsed((prev) => !prev);
@@ -22,6 +28,29 @@ export const Sidebar: FC<ISidebar> = ({ className }) => {
             <div className={`${styles.switchers} ${isCollapsed ? styles.switchersInCol : ''}`}>
                 <ThemeSwitcher />
                 <LangSwitcher short={isCollapsed} />
+            </div>
+
+            <div className={styles.appLinks}>
+                <AppLink
+                    to={RoutePath.main}
+                    theme={APP_LINK_THEME.SECONDARY}
+                    className={styles.appLink}
+                >
+
+                    <MainIcon className={styles.appLinkIcon} />
+                    <span className={styles.appLinkName}>{t('Main')}</span>
+                </AppLink>
+                <AppLink
+                    to={RoutePath.about}
+                    theme={APP_LINK_THEME.SECONDARY}
+                    className={styles.appLink}
+                >
+                    <AboutIcon className={styles.appLinkIcon} />
+                    <span className={styles.appLinkName}>
+                        {t('About')}
+                    </span>
+                </AppLink>
+
             </div>
 
             <Button
