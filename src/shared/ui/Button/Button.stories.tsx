6f988b85c2cad7a52d897077142d21b946258b9b
Viktoryia <viktoriabakun00@gmail.com>
2023-03-09 16:48:59 +0100

    feat: add variants of Button, add stories

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { THEME } from 'app/providers/ThemeProvider';
-import { Button, THEME_BUTTON } from '.';
+import { Button, BUTTON_SIZE, BUTTON_THEME } from '.';
 
 export default {
     title: 'shared/Button',
@@ -22,18 +22,74 @@ Primary.args = {
 export const Clear = Template.bind({});
 Clear.args = {
     children: 'Clear button',
-    theme: THEME_BUTTON.CLEAR,
+    theme: BUTTON_THEME.CLEAR,
 };
 
 export const BorderLight = Template.bind({});
 BorderLight.args = {
-    children: 'BorderLight',
-    theme: THEME_BUTTON.BORDER,
+    children: 'Border Light',
+    theme: BUTTON_THEME.BORDER,
 };
 
 export const BorderDark = Template.bind({});
 BorderDark.args = {
-    children: 'BorderDark',
-    theme: THEME_BUTTON.BORDER,
+    children: 'Border Dark',
+    theme: BUTTON_THEME.BORDER,
 };
 BorderDark.decorators = [ThemeDecorator(THEME.DARK)];
+
+export const BorderSizeM = Template.bind({});
+BorderSizeM.args = {
+    children: 'Size M',
+    theme: BUTTON_THEME.BORDER,
+    size: BUTTON_SIZE.M,
+};
+
+export const BorderSizeL = Template.bind({});
+BorderSizeL.args = {
+    children: 'Size L',
+    theme: BUTTON_THEME.BORDER,
+    size: BUTTON_SIZE.L,
+};
+
+export const BorderSizeXL = Template.bind({});
+BorderSizeXL.args = {
+    children: 'Size XL',
+    theme: BUTTON_THEME.BORDER,
+    size: BUTTON_SIZE.XL,
+};
+
+export const Background = Template.bind({});
+Background.args = {
+    children: 'Background',
+    theme: BUTTON_THEME.BACKGROUND,
+};
+
+export const BackgroundInverted = Template.bind({});
+BackgroundInverted.args = {
+    children: 'Background Inverted',
+    theme: BUTTON_THEME.BACKGROUND_INVERTED,
+};
+
+export const SquareSizeM = Template.bind({});
+SquareSizeM.args = {
+    children: '>',
+    theme: BUTTON_THEME.BACKGROUND_INVERTED,
+    square: true,
+};
+
+export const SquareSizeL = Template.bind({});
+SquareSizeL.args = {
+    children: '>',
+    theme: BUTTON_THEME.BACKGROUND_INVERTED,
+    square: true,
+    size: BUTTON_SIZE.L,
+};
+
+export const SquareSizeXL = Template.bind({});
+SquareSizeXL.args = {
+    children: '>',
+    theme: BUTTON_THEME.BACKGROUND_INVERTED,
+    square: true,
+    size: BUTTON_SIZE.XL,
+};
